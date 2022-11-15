357f05a0eafe16fd373a2149f615f4593cb7c8be
Edwardfaker <53597116+Edwardfaker@users.noreply.github.com>
2022-11-16 01:47:28 +0800

    Update push.ts

--- src/push.ts
+++ src/push.ts
@@ -36,6 +36,7 @@ export async function pushNotification(title: string, description: string) {
     if (!!sctKey) {
         const serverChanTurbo = new ServerChanTurbo(sctKey);
         promises.push(serverChanTurbo.send(title, description));
+        console.log("已发送server酱信息");
     }else{
          throw new Error("未向server酱发送信息");
     }
